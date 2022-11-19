daa6b74e090aeb429c28fc29c202b53967fff3c9
breaklunch <elias.ibanez@tecsup.edu.pe>
2022-11-18 22:50:58 -0500

    P.A

---
+++ Proyecto_API/api/views.py
@@ -0,0 +1,16 @@
+
+from django.views import View
+
+class CompanyView(View):
+    
+    def get(self,request):
+        pass
+
+    def post(self,request):
+        pass
+    
+    def put(self,request):
+        pass
+    
+    def delete(self,request):
+        pass
